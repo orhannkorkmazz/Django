d41d57bc5d4e1a962a4d0ff97b14e5688a3615ad
Orhan Korkmaz <orhann.korkmazz@gmail.com>
2023-09-29 22:55:39 +0300

    Add files via upload

--- user/views.py
+++ user/views.py
@@ -1,44 +1,53 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .forms import RegisterForm,LoginForm
 from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login, authenticate,logout
-
+# Create your views here.
 def register(request):
     form = RegisterForm(request.POST or None)
     if form.is_valid():
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password")
-            newUser = User(username =username)
-            newUser.set_password(password)
-            newUser.save()
-            #login(request,newUser)
-            messages.success(request,"Başarıyla kayıt oldunuz.")
-            return redirect("index") 
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        newUser = User(username =username)
+        newUser.set_password(password)
+
+        newUser.save()
+        #login(request,newUser)
+        messages.success(request, "Başarıyla kayıt oldunuz.")
+        
+
+        return redirect("index")
     else:
         context = {
             "form" : form
-        } 
+        }
     return render(request,"register.html",context)
 def loginUser(request):
-	if request.method == "POST":
-		form = AuthenticationForm(request, data=request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=password)
-			if user is not None:
-				login(request, user)
-				messages.success(request,"Başarıyla Giriş Yaptınız")
-				return redirect("index")
-			else:
-				messages.warning(request,"Kullanıcı adı veya parola hatalı.")
-		else:
-			messages.warning(request,"Kullanıcı adı veya parola hatalı.")
-	form = AuthenticationForm(request)
-	return render(request, "login.html", {"form":form})
+    form = LoginForm(request.POST or None)
+    context = {
+        "form":form
+    }
+
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        kullanıcı=authenticate(username=username,password=password)
+        if kullanıcı is None:
+            messages.info(request,"Böyle bir kullanıcı bulunmuyor ya da şifre yanlış")
+            return render(request,"login.html",context)
+        messages.success(request,"Başarıyla Giriş Yaptınız")
+        login(request,kullanıcı)
+        return redirect("index")
+    return render(request,"login.html",context)
 def logoutUser(request):
+    
     logout(request)
-    messages.success(request,"Başarıyla çıkış yaptınız.")
+    messages.info(request,"Başarıyla çıkış yaptınız" )
     return redirect('index')
+    
+
+
+
+
