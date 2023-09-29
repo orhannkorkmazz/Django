2a289cdb59242e40b71fae71d65e80f85582f5d0
Orhan Korkmaz <orhann.korkmazz@gmail.com>
2023-09-29 22:57:48 +0300

    Add files via upload

--- article/admin.py
+++ article/admin.py
@@ -1,13 +1,19 @@
 from django.contrib import admin
-from . models import Article,Comment
+from . models import Article,Comment,UserProfile
+from django.contrib import admin
+
+
+
 
 # Register your models here.
 admin.site.register(Comment)#Comment modelini form oluşturmadan kaydettim
+admin.site.register(UserProfile)
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_date')
+    list_display = ( 'title', 'author', 'created_date')
     list_display_links=['title']
     list_filter = ('created_date', 'author')
     search_fields = ('title', 'content')
     class Meta:
         model=Article
+
--- article/models.py
+++ article/models.py
@@ -9,6 +9,7 @@ class Article(models.Model):
     category=models.CharField(max_length=50,verbose_name=('Tür'),default="")
     created_date=models.DateTimeField(auto_now_add=True, verbose_name=('Oluşturulma Tarihi'))
     image = models.FileField(blank=True,null=True,verbose_name=('Resim Yükle'))#her makalenin dosyası yüklenmek zorunda değil
+    
     def __str__(self):
         return self.title
     CATEGORY_CHOICES = (
@@ -18,6 +19,7 @@ class Article(models.Model):
     ('Gazete', 'Gazete'),
     # Diğer kategori seçeneklerini buraya ekleyin
 )
+   
 class Comment(models.Model):
     title=models.ForeignKey(Article,on_delete=models.CASCADE,verbose_name="makale",related_name="comments")
     author=models.CharField(max_length=50, verbose_name=('Yazan'))
@@ -25,4 +27,8 @@ class Comment(models.Model):
     created_date=models.DateTimeField(auto_now_add=True, verbose_name=('Oluşturulma Tarihi'))
     def __str__(self):
         return self.content
-
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_picture', null=True, blank=True)
+    def __str__(self):
+        return self.user.username
